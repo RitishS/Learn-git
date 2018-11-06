e0fd6d3de4f87c04d0e9f1c9342c86cc49df85e2
RitishS <44412461+RitishS@users.noreply.github.com>
2018-11-06 23:40:25 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number to get double of its value : "))
+double = num*3                                                  #I know its wrong.
+print("Double of "+num+" is "+double+".")
